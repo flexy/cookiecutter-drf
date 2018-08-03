163b2788f0a0950b7315ec63056153df78d1a28d
Joshua Smith <thewanuki@gmail.com>
2018-08-03 13:37:43 -0700

    Fix deploy scripts

--- {{ cookiecutter.project_slug }}/travis/deploy_dev.sh
+++ {{ cookiecutter.project_slug }}/travis/deploy_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin "$REGISTRY"
-docker build -t "$DEV_URL" -f Dockerfile-prod .
-docker push "$DEV_URL"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin "$DEPLOY_DEV_REGISTRY_URL"
+docker build -t "$DEPLOY_DEV_APP_URL" -f Dockerfile-prod .
+docker push "$DEPLOY_DEV_APP_URL"
--- {{ cookiecutter.project_slug }}/travis/deploy_prod.sh
+++ {{ cookiecutter.project_slug }}/travis/deploy_prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin "$REGISTRY"
-docker build -t "$PROD_URL" -f Dockerfile-prod .
-docker push "$PROD_URL"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin "$DEPLOY_PROD_REGISTRY_URL"
+docker build -t "$DEPLOY_PROD_APP_URL" -f Dockerfile-prod .
+docker push "$DEPLOY_PROD_APP_URL"
