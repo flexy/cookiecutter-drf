040fe318ba3b30b2870f7e1e8a529a99c7b50045
Josh Smith <josh@joshsmith.codes>
2018-08-17 12:30:44 -0700

    Refactor VersionMixin and add tests

---
+++ {{ cookiecutter.project_slug }}/api/utils/tests/test_viewsets.py
@@ -0,0 +1,101 @@
+import pytest
+import builtins
+
+from ..versions.viewsets import VersionMixin
+
+
+class TestVersionMixin():
+    def _setup(self, mocker):
+        # Setup class instance
+        instance = VersionMixin()
+
+        # Setup attributes
+        instance.filterset_classes = {}
+        instance.serializer_classes = {}
+        instance.request = mocker.Mock()
+
+        return instance
+
+    def test_get_versioned_class(self, mocker):
+        instance = self._setup(mocker)
+
+        # Save values for later
+        filterset_1 = 'FilterSet1'
+        filterset_default = 'FilterSetDefault'
+
+        # Check cases
+        # No default, version not specified
+        instance.request.version = None
+
+        with pytest.raises(KeyError):
+            instance._get_versioned_class('filterset_classes')
+
+        # No default, version not found
+        instance.request.version = '100.0'
+
+        with pytest.raises(KeyError):
+            instance._get_versioned_class('filterset_classes')
+
+        # No default, version specified
+        instance.request.version = '1.0'
+        instance.filterset_classes['1.0'] = filterset_1
+
+        instance._get_versioned_class('filterset_classes') == filterset_1
+
+        # Default, version not specified
+        instance.request.version = None
+        instance.filterset_classes['default'] = filterset_default
+
+        assert instance._get_versioned_class('filterset_classes') == filterset_default
+
+        # Default, version not found
+        instance.request.version = '100.0'
+
+        assert instance._get_versioned_class('filterset_classes') == filterset_default
+
+        # Default, version specified
+        instance.request.version = '1.0'
+
+        assert instance._get_versioned_class('filterset_classes') == filterset_1
+
+    def test_get_filterset_class(self, mocker):
+        instance = self._setup(mocker)
+
+        # Patch methods
+        versioned_class = mocker.Mock()
+        instance._get_versioned_class = versioned_class
+
+        # Check cases
+        # Runs without error
+        instance.get_filterset_class()
+
+        versioned_class.assert_called_with('filterset_classes')
+
+        # filterset_classes not found
+        versioned_class.side_effect = AttributeError
+
+        with pytest.raises(AttributeError) as error_info:
+            instance.get_filterset_class()
+
+        assert error_info.value.args[0] == "'super' object has no attribute 'get_filterset_class'"
+
+    def test_get_serializer_class(self, mocker):
+        instance = self._setup(mocker)
+
+        # Patch methods
+        versioned_class = mocker.Mock()
+        instance._get_versioned_class = versioned_class
+
+        # Check cases
+        # Runs without error
+        instance.get_serializer_class()
+
+        versioned_class.assert_called_with('serializer_classes')
+
+        # serializer_classes not found
+        versioned_class.side_effect = AttributeError
+
+        with pytest.raises(AttributeError) as error_info:
+            instance.get_serializer_class()
+
+        assert error_info.value.args[0] == "'super' object has no attribute 'get_serializer_class'"
--- {{ cookiecutter.project_slug }}/api/utils/versions/viewsets.py
+++ {{ cookiecutter.project_slug }}/api/utils/versions/viewsets.py
@@ -1,19 +1,31 @@
 class VersionMixin(object):
     def _get_versioned_class(self, attribute):
-        return getattr(self, attribute)[self.request.version]
+        attribute = getattr(self, attribute)
+        version = self.request.version
+
+        # Return default if version is not specified
+        if not version:
+            return attribute['default']
+
+        try:
+            # Return version
+            return attribute[version]
+        except KeyError:
+            # Return default if version not found
+            return attribute['default']
 
     def get_filterset_class(self):
-        # If filterset_classes is not set, don't do anything
-        if not hasattr(self, 'filterset_classes'):
+        try:
+            # Return appropriate filterset for version
+            return self._get_versioned_class('filterset_classes')
+        except AttributeError:
+            # If filterset_classes is not set, don't do anything
             return super().get_filterset_class()
 
-        # Return appropriate filterset for version
-        return self._get_versioned_class('filterset_classes')
-
     def get_serializer_class(self):
-        # If serializer_classes is not set, don't do anything
-        if not hasattr(self, 'serializer_classes'):
+        try:
+            # Return appropriate serializer for version
+            return self._get_versioned_class('serializer_classes')
+        except AttributeError:
+            # If serializer_classes is not set, don't do anything
             return super().get_serializer_class()
-
-        # Return appropriate serializer for version
-        return self._get_versioned_class('serializer_classes')
